1ab8474fe3ccd5780ad53074ef96a3029605ca99
张龙佳 <zhanglongjia@corp.netease.com>
2021-11-25 10:07:48 +0800

    fixed import

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,7 @@
 const program = require('commander');
 const download = require('download-git-repo');
 const chalk = require('chalk');
-const ora = require('ora');
+import ora from 'ora';
 // import packageVersion from './utils/packageVersion';
 
 async function run() {
